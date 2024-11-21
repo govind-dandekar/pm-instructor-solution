97cca4c11ffd760f83fc68521db6e619955cca7d
Govind Dandekar <govind@Govinds-Mini.lan>
2024-11-21 10:57:07 -0800

    add Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+function Input({ label, isTextArea, ...props }){
+	return(
+		<p>
+			<label>{label}</label>
+			{isTextArea ? 
+				<textarea {...props} /> :
+				<input {...props} />
+			}
+		</p>
+	)
+}
+
+export default Input;
---
+++ src/components/NewProject.jsx
@@ -0,0 +1,26 @@
+import Input from "./INput";
+
+function NewProject(){
+	return(
+		<div>
+			<menu>
+				<li><button>Cancel</button></li>
+				<li><button>Save</button></li>
+			</menu>
+			<div>
+				<Input
+					label="Title"
+				/>
+				<Input 
+					label="Description"
+					isTextArea
+				/>
+				<Input 
+					label="Due Date"
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default NewProject;
